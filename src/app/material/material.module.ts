8ace336658c36ae72de837b474f33ece2cfbfd4b
genesis-bit <2018genesio@gmail.com>
2023-08-28 16:38:37 +0100

    Add flexLayout e Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+//const MaterialComponentes = [];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+ //exports: [MaterialComponentes],
+})
+export class MaterialModule { }
